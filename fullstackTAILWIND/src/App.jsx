bfe7b64dc0aaad191f6f0b24ca20da71e0e3ea11
ATLAS-B28 <adityacbhambere28@gmail.com>
2023-04-30 19:55:11 +0530

    Tailwind css

---
+++ fullstackTAILWIND/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+       <h1 className="font-bold text-3xl text-center underline text-red-500
+             hover:text-blue-700"
+        >
+        Hello World
+       </h1>
+       <button
+        className='border-2 
+        px-2 py-1 
+        border-blue-500
+        rounded 
+        bg-blue-500
+        text-white
+        hover:bg-transparent
+        hover:text-blue-500
+        '
+       >This is button</button>
+    </>
+  )
+}
+
+export default App
