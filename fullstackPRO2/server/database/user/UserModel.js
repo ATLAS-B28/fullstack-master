b576502278a48522af8363dbb064e550019415d6
ATLAS-B28 <adityacbhambere28@gmail.com>
2023-05-03 19:55:04 +0530

    Added schema

---
+++ fullstackPRO2/server/database/user/UserModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+const UserScehma = new mongoose.Schema({
+    fullname:{
+        type:String,required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String
+    },
+    address:[{
+        detail:{
+            type:String
+        },
+        for:{
+            type:String
+        }
+    }],
+    contact:[{
+        type:String
+    }]
+},{
+    timestamps:true
+})
+export const UserModel =  mongoose.model("Users",UserScehma)
